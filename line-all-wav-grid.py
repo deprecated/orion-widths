8d429e969df5c57a2444281305c00fe2d714740d
William Henney <will@henney.org>
2016-12-24 08:46:33 +0000

    More fiddling with the all-line program

--- line-all-wav-grid.py
+++ line-all-wav-grid.py
@@ -27,11 +27,16 @@ for row in linetab:
     label = '{} {:.2f}'.format(row['Ion'], wav0)
     netflux = (tab['flux'] - tab['cont'])/tab['cont']
     mask4 = np.abs(tab['wav'] - wav0) < 4.0
-    mask1 = np.abs(tab['wav'] - wav0) < 1.0
+    mask2 = np.abs(tab['wav'] - wav0) < 2.0
+    mask_blue = np.abs(tab['wav'] - (wav0 - 6.0)) < 2.0
+    mask_red = np.abs(tab['wav'] - (wav0 + 6.0)) < 2.0
+    margin = 0.2*max(netflux[mask2].max(), -netflux.min())
+    ymin = netflux.min() - margin
+    ymax = max(0.0, netflux[mask2].max()) + 4*margin
     ax = next(flaxes)
     ax.plot(tab['wav'], netflux, label=label, c='r')
     ax.axhline(0.0,  ls='--', c='b')
-    if netflux[mask1].sum() > 0.0:
+    if netflux[mask2].sum() > 0.0:
         # Emission line
         fillcolor = 'r'
     else:
@@ -39,13 +44,17 @@ for row in linetab:
         fillcolor = 'b'
     ax.fill_between(tab['wav'], netflux, where=mask4,
                     color=fillcolor, alpha=0.3)
+    if row['blue cont']:
+        ax.fill_between(tab['wav'], netflux, ymin,
+                        where=mask_blue, color='k', alpha=0.1)
+    if row['red cont']:
+        ax.fill_between(tab['wav'], netflux, ymin,
+                        where=mask_red, color='k', alpha=0.1)
     ax.axvline(wav0, ls='--', color='k')
-    ax.legend(loc='best')
-    margin = 0.2*max(netflux[mask1].max(), -netflux.min())
+    ax.legend(loc='best', frameon=True, framealpha=0.8)
     ax.set(
         xlim=[tab['wav'].min(), tab['wav'].max()],
-        ylim=[netflux.min() - margin,
-	      max(0.0, netflux[mask1].max()) + 3*margin],
+        ylim=[ymin, ymax],
     )
     ax.xaxis.set_major_locator(MultipleLocator(5))
     ax.yaxis.set_major_locator(MaxNLocator(7))
@@ -56,4 +65,4 @@ axes[-1, 0].set(
 
 fig.tight_layout()
 fig.savefig(figfile)
-print(figfile)
+print(figfile, end='')
